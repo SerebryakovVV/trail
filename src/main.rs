4f2de09051adb3532068c441eb43bf6a731444b3
SerebryakovVV <serebryakov.valentin@gmail.com>
2025-07-31 21:44:45 +0700

    fading trail completed

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,14 @@
 // arc website pixel trail
 // Bresenham's Line Algorithm
-// use |stack - circular buffer| for the pixels with color
-// on update run old to new, so if a pixel is revisited, we just have the latest value
-// and we have two pointers, the oldest time-ended value will move pointer on its death
 
 mod trail;
 use trail::Trail;
-
 use raylib::prelude::*;
-
 
 
 const CELL_SIZE    : i32   = 20 ;
 const FIELD_WIDTH  : usize = 30 ;
 const FIELD_HEIGHT : usize = 30 ;
-
-
-
 
 
 fn mouse_pos_to_field(pos: Vector2) -> (i32, i32) {
@@ -26,13 +18,9 @@ fn mouse_pos_to_field(pos: Vector2) -> (i32, i32) {
   )
 }
 
-fn draw_field(handle: &mut RaylibDrawHandle, trail: &Vec<(i32, i32)>) {
-  for rec in trail.iter() {
-    handle.draw_rectangle(rec.0, rec.1, CELL_SIZE, CELL_SIZE, Color::BLACK);
-  }
-}
 
 fn bresenham() {}
+
 
 enum DrawMode {
   Click,
@@ -41,9 +29,8 @@ enum DrawMode {
 
 
 fn main() {
-  let mut draw_mode = DrawMode::Hover;
+  let draw_mode = DrawMode::Hover;
   let mut prev_mouse_pos_in_field = (0, 0);
-  // let mut trail: Vec<(i32, i32)> = Vec::new();
   let mut trail = Trail::new();
   let (mut rl, thread) = raylib::init()
                                 .size(
@@ -53,16 +40,9 @@ fn main() {
                                 .build();
   rl.set_target_fps(60);
   while !rl.window_should_close() {
- 
-    //    if d.is_key_pressed(KeyboardKey::KEY_ENTER) {
-    //     edit_flag = if edit_flag {false} else {true};
-    // }
-
-    
- 
     let mut d = rl.begin_drawing(&thread);
+    d.clear_background(Color::WHITE);
     let time_now = d.get_time();
-
     match draw_mode {
       DrawMode::Click => {
         if d.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {
@@ -76,28 +56,13 @@ fn main() {
       DrawMode::Hover => {
         let new_pos = mouse_pos_to_field(d.get_mouse_position());
         if new_pos != prev_mouse_pos_in_field {
-          prev_mouse_pos_in_field = new_pos;                         // the trail grows when hovering over the same pixels, but thats what i need, actually
+          prev_mouse_pos_in_field = new_pos;
           trail.set_active(true)
-               .ignite(&mut d, new_pos.0, new_pos.1, time_now)
-               .bury(&mut d, time_now)
-               .color(&mut d);                                         // the better way would be to check for mouse move separately
-        }                                                              // and to bury on each frame
+               .ignite(&mut d, new_pos.0, new_pos.1, time_now);                                           
+        }
       },
     }
-
-    
-
-
-    d.clear_background(Color::WHITE);
-    // draw_field(&mut d, &trail);
-
-    // let x = d.get_mouse_position();
- 
-  
-    // if d.is_mouse_button_pressed(MouseButton::MOUSE_BUTTON_LEFT) {}
-
-    
-    
-
+    trail.bury(&mut d, time_now)
+         .color(&mut d, time_now); 
   }
 }
--- src/trail.rs
+++ src/trail.rs
@@ -1,8 +1,8 @@
 use raylib::{color::Color, prelude::{RaylibDraw, RaylibDrawHandle}};
-
 use crate::CELL_SIZE;
 
-const TRAIL_SIZE: usize = 5;
+const TRAIL_SIZE: usize = 60;
+const LIFE_TIME: f64 = 1.0;
 
 // https://doc.rust-lang.org/std/num/type.NonZeroUsize.html
 
@@ -43,46 +43,31 @@ impl Trail {
     self
   }
 
-  pub fn bury(&mut self, handle: &mut RaylibDrawHandle, time: f64) -> &mut Self {
+  pub fn bury(&mut self, handle: &mut RaylibDrawHandle, time_now: f64) -> &mut Self {
     let Slot {x, y, birth} = self.data[self.start];
-
-    if birth < time && self.active { // can use recursion here, but need to be careful, cause it will loop infinitely; maybe should add did_ignite flag, so this would quaranty there is at leas one cell alive
-      
+    if time_now - birth > LIFE_TIME && self.active {
       handle.draw_rectangle(x, y, CELL_SIZE, CELL_SIZE, Color::WHITE);
       if self.start == self.end {
         self.set_active(false);
-        
-        return self         // rewrite to non-recursive
+        return self
       }
-
-      
-      // paint cell white
-      // check for overflow
-
       if self.start >= self.data.len() - 1 {
         self.start = 0;
       } else {
         self.start += 1;
       }
-                      // well, i have the end index, so there is actually nothing to be afraid of with recursion
-      
-      
-        return self.bury(handle, time);         // rewrite to non-recursive
-      
-
+      return self.bury(handle, time_now);
     }
     self
   }
 
-  // if there is too many new cells, then end can reach start
-  // need to implement a kill_oldest method
   pub fn ignite(&mut self, handle: &mut RaylibDrawHandle, x: i32, y: i32, birth: f64) -> &mut Self {
-    if self.end >= self.data.len() - 1 {                                // if the length of the ring is zero, this panics, because usize
+    if self.end >= self.data.len() - 1 {
       self.end = 0;
     } else {
       self.end += 1;
     }
-    if self.end == self.start && self.active {                          // also check for active here, will run when the trail is empty
+    if self.end == self.start && self.active {
       self.kill_oldest(handle);
     }
     let slot_to_enter = &mut self.data[self.end];
@@ -102,16 +87,21 @@ impl Trail {
     }
   }
 
-
-  // it would be better to separate this into another function, outside of ring struct
-  // pub fn color(mut self) -> Self {
-  pub fn color(&mut self, handle: &mut RaylibDrawHandle) -> &mut Self {
-    // this will run at the end
-    // check the time on each slot
-    // color accordingly
+  pub fn color(&mut self, handle: &mut RaylibDrawHandle, time_now: f64) -> &mut Self {
+    let mut index = self.start;
+    loop { 
+      let Slot { x, y, birth } = self.data[index];
+      let alpha = (255.0 * (1.0 - (time_now - birth) / LIFE_TIME)) as u8;
+      handle.draw_rectangle(x, y, CELL_SIZE, CELL_SIZE, Color::new(0, 0, 0, alpha));
+      if index == self.end {break;}
+      if index >= self.data.len() - 1 {
+        index = 0;
+      } else {
+        index += 1;
+      }
+    }
     self
   }
-
 }
 
 
@@ -120,46 +110,5 @@ mod tests {
   use super::*;
 
   #[test]
-  fn test_ignition() {
-    // let mut ring = Ring::new();
-    // ring.ignite((0,0,0.5));
-    // ring.ignite((0,0,1.0));
-    // ring.ignite((0,0,1.5));
-    // ring.ignite((0,0,2.0));
-    // ring.ignite((0,0,2.5));
-    // ring.ignite((0,0,3.0));
-    // println!("{:#?}", ring);
-    // ring.bury(0.1);
-    // println!("{:?}", ring);
-    // ring.bury(0.2);
-    // println!("{:?}", ring);
-    // ring.bury(0.6);
-    // println!("{:?}", ring);
-    // ring.bury(2.2);
-    // println!("{:?}", ring);
-    
-
-  }
+  fn test_ignition() {}
 }
-
-// on each frame we check the oldest link
-// if dead, move the start further, check next one, cycle
-// then we check if mouse moved out of previous cell
-// if it did, then the main part of the program begins and we extrapolate the pixels
-// push new pixel into ring, with smoothed birth timestamp
-// move end too
-// and the indexes can move around the ring, thats why its called ring btw
-
-// additions:
-// color the slot that just died white
-//    this implies that we bury first and color second
-
-
-
-// whats the state of the field, if end=start, or if end=0
-
-
-
-
-
-
